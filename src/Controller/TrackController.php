736a4c40c266f5fceccb58214544ba22e04ea6cd
FRERE Eoten <eoten.frere@etu.univ-lyon1.fr>
2025-10-07 14:52:50 +0200

    display titles

--- src/Controller/TrackController.php
+++ src/Controller/TrackController.php
@@ -3,26 +3,41 @@
 namespace App\Controller;
 
 use App\Service\AuthSpotifyService;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[Route('/track')]
 
 class TrackController extends AbstractController
 {
     private string $token;
+    private HttpClientInterface $httpClient;
     public function __construct(private readonly AuthSpotifyService $authSpotifyService,)
     {
         $this->token = $this->authSpotifyService->auth();
-        dd($this->token);
+        $this->httpClient = HttpClient::create();
     }
 
     #[Route('/', name: 'app_track_index')]
-
-
-    public function number(int $max): Response
+    public function index(): Response
     {
-        return $this->render('track/track.html.twig');
+        $response = $this->httpClient->request('GET', 'https://api.spotify.com/v1/search?offset=0&limit=20&query=freeze%20corleone&type=track&locale=fr-FR',
+        [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $this->token,
+            ]
+        ])->toArray();
+
+        return $this->render('track/index.html.twig', [
+            'controller_name' => 'TrackController',
+            'data' => $response['tracks']['items'],
+        ]);
     }
+
+
+
 }
