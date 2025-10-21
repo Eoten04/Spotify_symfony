edad4e654f2300e155077f2c01aab6e0f7a928ab
FRERE Eoten <eoten.frere@etu.univ-lyon1.fr>
2025-10-21 09:33:38 +0200

    display titles

--- src/Controller/TrackController.php
+++ src/Controller/TrackController.php
@@ -1,57 +1,45 @@
 <?php
-// src/Controller/LuckyController.php
+
 namespace App\Controller;
 
 use App\Service\AuthSpotifyService;
-use Symfony\Component\HttpClient\HttpClient;
+use App\Service\SpotifyRequestService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[Route('/track')]
-
 class TrackController extends AbstractController
 {
     private string $token;
-    private HttpClientInterface $httpClient;
-    public function __construct(private readonly AuthSpotifyService $authSpotifyService,)
+
+    public function __construct(
+        private readonly AuthSpotifyService    $authSpotifyService,
+        private readonly SpotifyRequestService $spotifyRequestService
+    )
     {
         $this->token = $this->authSpotifyService->auth();
-        $this->httpClient = HttpClient::create();
     }
 
-    #[Route('/', name: 'app_track')]
+    #[Route('/', name: 'app_track_index')]
     public function index(): Response
     {
-
         return $this->render('track/index.html.twig');
     }
 
-    #[Route('/response', name: 'app_track_response', methods: ['GET', 'POST'])]
-    public function response(Request $request): Response
+    #[Route('/show', name: 'app_track_show')]
+    public function show(Request $request): Response
     {
-        $artist = $request->request->get('search');
-
-        if (!$artist) {
-            return $this->redirectToRoute('app_track');
+        $search = $request->query->get('search');
+        $results = [];
+        if ($search) {
+            $results = $this->spotifyRequestService->searchTracks($search, $this->token);
         }
 
-        $response = $this->httpClient->request(
-            'GET',
-            'https://api.spotify.com/v1/search?query=' . urlencode($artist) . '&type=track&locale=fr_FR',
-            [
-                'headers' => [
-                    'Authorization' => 'Bearer ' . $this->token,
-                ],
-            ]
-        )->toArray();
-        $data = $response['tracks']['items'] ?? [];
-//dd($data);
-        return $this->render('track/response.html.twig', [
-            'data' => $data,
-            'artist' => $artist,
+        return $this->render('track/show.html.twig', [
+            'results' => $results,
+            'search' => $search,
         ]);
-        }
+    }
 }
